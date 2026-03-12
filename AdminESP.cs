404578d32c2b0023443ba934ebdf5daea95ef2d0
Qazi Abdullah <60501013+qaziabdullah@users.noreply.github.com>
2026-03-12 07:36:00 +0500

    Refactor AdminESP module and toggle logic
    Updated module name, author, and version. Refactored ESP toggle logic for admin players.

--- AdminESP.cs
+++ AdminESP.cs
@@ -1,4 +1,4 @@
-﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Admin;
@@ -10,9 +10,9 @@ namespace AdminESP;
 
 public sealed partial class AdminESP : BasePlugin, IPluginConfig<Config>
 {
-    public override string ModuleName => "Admin ESP";
-    public override string ModuleAuthor => "AquaVadis & GSM-RO";
-    public override string ModuleVersion => "1.3.1"; 
+    public override string ModuleName => "CSXRAY";
+    public override string ModuleAuthor => "firekickfly;
+    public override string ModuleVersion => "1.0"; 
     public override string ModuleDescription => "Plugin uses code borrowed from CS2Fixes / cs2kz-metamod / hl2sdk / unknown cheats and xstage from CS# discord";
 
     public bool[] toggleAdminESP = new bool[64]; 
@@ -41,166 +41,79 @@ public sealed partial class AdminESP : BasePlugin, IPluginConfig<Config>
 
     public override void Unload(bool hotReload)
     {
-
         DeregisterListeners();
+    }
+
+    private void ApplyESPToggle(CCSPlayerController player, bool enabled)
+    {
+        toggleAdminESP[player.Slot] = enabled;
+
+        if (enabled) {
+            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true)
+                SetAllPlayersGlowing();
+        }
+        else {
+            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true)
+                RemoveAllGlowingPlayers();
+        }
     }
 
     [ConsoleCommand("css_esp", "Toggle Admin ESP (non-persistent)")]
     [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
     public void OnToggleAdminEsp(CCSPlayerController? player, CommandInfo command)
     {
-
         if (player is null || player.IsValid is not true) return;
 
         if (AdminManager.PlayerHasPermissions(player, Config.AdminFlag) is not true) {
-
             SendMessageToSpecificChat(player, msg: "Admin ESP can only be used from {GREEN}admins{DEFAULT}!", print: PrintTo.Chat);
             return;  
         }
-        
-        switch (player.PawnIsAlive) {
-            
-            case true:
 
-                if (Config.AllowDeadAdminESP is true) {
-                    SendMessageToSpecificChat(player, msg: "You should be {RED}dead {DEFAULT}to use Admin ESP!", print: PrintTo.Chat);
-                    return;
-                }
-                SendMessageToSpecificChat(player, msg: "Admin ESP is only allowed while {RED}spectating{DEFAULT}!", print: PrintTo.Chat);
-
-            break;
-            case false:
-
-                if (player.Team is CsTeam.Spectator) {
-                    toggleAdminESP[player.Slot] = !toggleAdminESP[player.Slot];
-
-                    if (toggleAdminESP[player.Slot] is true) {
-                        
-                        if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                            SetAllPlayersGlowing();
-                        }
-                        
-                    }
-                    else {
-
-                        if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                            RemoveAllGlowingPlayers();
-                        }
-                        
-                    }
-                    
-
-                    SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat); 
-                    return;
-                }
-                else {
-                    if (Config.AllowDeadAdminESP is true) {
-                        toggleAdminESP[player.Slot] = !toggleAdminESP[player.Slot];
-
-                        if (toggleAdminESP[player.Slot] is true) {
-
-                            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                                SetAllPlayersGlowing();
-                            }
-                            
-                        }
-                        else {
-
-                            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                                RemoveAllGlowingPlayers();
-                            }
-                            
-                        }
-
-                        SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
-                        return;
-                    
-                    }
-                }
-
-                SendMessageToSpecificChat(player, msg: "Admin ESP is only allowed in {RED}spectate mode{DEFAULT}!", print: PrintTo.Chat);
-            break;
-        
-
+        // Allow spectators always
+        if (player.Team is CsTeam.Spectator) {
+            ApplyESPToggle(player, !toggleAdminESP[player.Slot]);
+            SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
+            return;
         }
+
+        // Allow alive or dead (non-spectator) admins if config permits
+        if (Config.AllowDeadAdminESP is true) {
+            ApplyESPToggle(player, !toggleAdminESP[player.Slot]);
+            SendMessageToSpecificChat(player, msg: $"Admin ESP has been " + (toggleAdminESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
+            return;
+        }
+
+        SendMessageToSpecificChat(player, msg: "Admin ESP is only allowed in {RED}spectate mode{DEFAULT}!", print: PrintTo.Chat);
     }
 
     [ConsoleCommand("css_esp_p", "Toggle Persistent Admin ESP")]
     [CommandHelper(minArgs: 0, whoCanExecute: CommandUsage.CLIENT_ONLY)]
     public void OnTogglePersistentAdminEsp(CCSPlayerController? player, CommandInfo command)
     {
-
         if (player is null || player.IsValid is not true) return;
 
         if (AdminManager.PlayerHasPermissions(player, Config.AdminFlag) is not true) {
-
             SendMessageToSpecificChat(player, msg: "Persistent Admin ESP can only be used from {GREEN}admins{DEFAULT}!", print: PrintTo.Chat);
             return;  
         }
 
         wantESP[player.Slot] = !wantESP[player.Slot];
-        
-        switch (player.PawnIsAlive) {
-            
-            case true:
 
-                if (Config.AllowDeadAdminESP is true) {
-                    SendMessageToSpecificChat(player, msg: "You should be {RED}dead {DEFAULT}to use Persistent Admin ESP!", print: PrintTo.Chat);
-                    return;
-                }
-                SendMessageToSpecificChat(player, msg: "Persistent Admin ESP is only allowed while {RED}spectating{DEFAULT}!", print: PrintTo.Chat);
-
-            break;
-            case false:
-
-                if (player.Team is CsTeam.Spectator) {
-                    toggleAdminESP[player.Slot] = wantESP[player.Slot]; 
-
-                    if (toggleAdminESP[player.Slot] is true) {
-                        
-                        if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                            SetAllPlayersGlowing();
-                        }
-                    }
-                    else {
-
-                        if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                            RemoveAllGlowingPlayers();
-                        }
-                         }
-                    
-
-                    SendMessageToSpecificChat(player, msg: $"Persistent Admin ESP has been " + (wantESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat); 
-                    return;
-                }
-                else {
-                    if (Config.AllowDeadAdminESP is true) {
-                        toggleAdminESP[player.Slot] = wantESP[player.Slot]; 
-
-                        if (toggleAdminESP[player.Slot] is true) {
-
-                            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                                SetAllPlayersGlowing();
-                            }
-                        }
-                        else {
-
-                            if (togglePlayersGlowing is not true || AreThereEsperingAdmins() is not true) {
-                                RemoveAllGlowingPlayers();
-                            }
-                        }
-
-                        SendMessageToSpecificChat(player, msg: $"Persistent Admin ESP has been " + (wantESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
-                        return;
-                    
-                    }
-                }
-
-                SendMessageToSpecificChat(player, msg: "Persistent Admin ESP is only allowed in {RED}spectate mode{DEFAULT}!", print: PrintTo.Chat);
-            break;
-        
-
+        // Allow spectators always
+        if (player.Team is CsTeam.Spectator) {
+            ApplyESPToggle(player, wantESP[player.Slot]);
+            SendMessageToSpecificChat(player, msg: $"Persistent Admin ESP has been " + (wantESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
+            return;
         }
+
+        // Allow alive or dead (non-spectator) admins if config permits
+        if (Config.AllowDeadAdminESP is true) {
+            ApplyESPToggle(player, wantESP[player.Slot]);
+            SendMessageToSpecificChat(player, msg: $"Persistent Admin ESP has been " + (wantESP[player.Slot] ? "{GREEN}enabled!" : "{RED}disabled!"), print: PrintTo.Chat);
+            return;
+        }
+
+        SendMessageToSpecificChat(player, msg: "Persistent Admin ESP is only allowed in {RED}spectate mode{DEFAULT}!", print: PrintTo.Chat);
     }
 
     public void OnConfigParsed(Config config)
